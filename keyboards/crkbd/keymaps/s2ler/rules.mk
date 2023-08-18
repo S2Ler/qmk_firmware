7405bdf970f7e173da4cdae92602a2fceb1d41f9
Aliaksandr Bialiauski <413986+S2Ler@users.noreply.github.com>
2023-08-18 13:07:24 +0300

    Add basic crkbd setup

---
+++ keyboards/crkbd/keymaps/s2ler/rules.mk
@@ -0,0 +1,7 @@
+OLED_ENABLE = yes
+OLED_DRIVER = SSD1306
+MOUSEKEY_ENABLE     = yes     # Mouse keys
+RGBLIGHT_ENABLE     = no    # Enable WS2812 RGB underlight.
+VIA_ENABLE          = no         # Enable VIA
+LTO_ENABLE          = yes
+CONSOLE_ENABLE = yes
