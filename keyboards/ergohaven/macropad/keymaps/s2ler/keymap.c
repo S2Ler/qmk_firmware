ede4a4d1e491b6706b428832d01abf98e396f09d
Aliaksandr Bialiauski <413986+S2Ler@users.noreply.github.com>
2023-11-25 06:43:43 +0300

    F layer for macropad

--- keyboards/ergohaven/macropad/keymaps/s2ler/keymap.c
+++ keyboards/ergohaven/macropad/keymaps/s2ler/keymap.c
@@ -24,14 +24,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_MYCM, KC_WSCH, KC_MAIL,
         KC_MPRV, KC_MPLY, KC_MNXT,
         TD_SAFE_BOOT, LAYER_PREV, LAYER_NEXT
-  ),
+        ),
     [XCODE_LAYER] = LAYOUT(
         KC_MUTE,
-        KC_7, KC_8,       KC_9,
-        KC_4, KC_5,       KC_6,
-        KC_1, KC_2,       KC_3,
+        KC_7, KC_8, KC_9,
+        KC_4, KC_5, KC_6,
+        KC_1, KC_2, KC_3,
         KC_0, _______, _______
-  ),
+        ),
+    [F_LAYER] = LAYOUT(
+        QK_BOOTLOADER,
+        KC_F7, KC_F8, KC_F9,
+        KC_F4, KC_F5, KC_F6,
+        KC_F1, KC_F2, KC_F3,
+        KC_F10, _______, _______
+        )
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
@@ -43,9 +50,9 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                 return false;
             }
 
-            uint8_t this_layer  = get_highest_layer(layer_state);
+            uint8_t this_layer = get_highest_layer(layer_state);
 
-            // Check if we are within the range, if not quit
+        // Check if we are within the range, if not quit
             if (this_layer > LAYER_CYCLE_END || this_layer < LAYER_CYCLE_START) {
                 return false;
             }
@@ -65,7 +72,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
             uint8_t current_layer = get_highest_layer(layer_state);
 
-            // Check if we are within the range, if not quit
+        // Check if we are within the range, if not quit
             if (current_layer > LAYER_CYCLE_END || current_layer < LAYER_CYCLE_START) {
                 return false;
             }
@@ -85,15 +92,16 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
 #ifdef ENCODER_MAP_ENABLE
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
-    [MEDIA_LAYER] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
-    [XCODE_LAYER] = { ENCODER_CCW_CW(KC_PGDN, KC_PGUP) },
+    [MEDIA_LAYER] = {ENCODER_CCW_CW(KC_VOLD, KC_VOLU)},
+    [XCODE_LAYER] = {ENCODER_CCW_CW(KC_PGDN, KC_PGUP)},
+    [F_LAYER] = {ENCODER_CCW_CW(KC_F11, KC_F12)},
 };
 #endif
 
 #ifdef OLED_ENABLE
 
 oled_rotation_t oled_init_user(oled_rotation_t rotation) {
-	return OLED_ROTATION_90;
+    return OLED_ROTATION_90;
 }
 
 bool oled_task_user(void) {
@@ -116,11 +124,18 @@ bool oled_task_user(void) {
             oled_write_ln_P(PSTR("1 2 3"), false);
             oled_write_ln_P(PSTR("0 LaP LaN"), false);
             break;
+        case F_LAYER:
+            oled_write_ln_P(PSTR("F Layer"), false);
+            oled_write_ln_P(PSTR("R 11 12"), false);
+            oled_write_ln_P(PSTR("7 8 9"), false);
+            oled_write_ln_P(PSTR("4 5 6"), false);
+            oled_write_ln_P(PSTR("1 2 3"), false);
+            oled_write_ln_P(PSTR("10 LaP LaN"), false);
         default:
             oled_write_ln_P(PSTR("Undef"), false);
     }
 
-  return true;
+    return true;
 }
 
 #endif
