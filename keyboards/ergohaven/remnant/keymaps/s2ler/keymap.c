40d9be227c044d0a3941afad0ec5069d09103b12
Aliaksandr Bialiauski <413986+S2Ler@users.noreply.github.com>
2023-11-26 07:35:31 +0300

    Move shortcut

--- keyboards/ergohaven/remnant/keymaps/s2ler/keymap.c
+++ keyboards/ergohaven/remnant/keymaps/s2ler/keymap.c
@@ -71,7 +71,7 @@ enum custom_codes {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [HOME_LAYER] = LAYOUT(
-        LGUI(KC_TAB), XXXXXXX, SH_RAW_PASTER, REDO, ACT_SELECTION_SCREENSHOT, TD(TD_SAFE_BOOT),     TD(TD_SAFE_BOOT), LANG_SWITCH, XXXXXXX, XXXXXXX, XXXXXXX, ACT_SWITCH_WINDOWS,
+        LGUI(KC_TAB), XXXXXXX, SH_RAW_PASTER, REDO, ACT_SELECTION_SCREENSHOT, TD(TD_SAFE_BOOT),     TD(TD_SAFE_BOOT), LANG_SWITCH, ACT_SHORTCAT, XXXXXXX, XXXXXXX, XXXXXXX,
 
         KC_TAB,  KC_Q,  KC_W, KC_E,  KC_R, KC_T,                                                    KC_Y, KC_U, KC_I, KC_O, KC_P, OSL(ACTION_LAYER),
         KC_LCTL, KC_A, KC_S, KC_D, KC_F, KC_G,                                                      KC_H, KC_J, KC_K, KC_L, LSFT_T(KC_ESC), LSFT(KC_RSFT),
@@ -114,7 +114,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         XXXXXXX, APP_PYCHARM, APP_BROWSER, XXXXXXX, ACT_SIRI, ACT_DICTATION,  ACT_PREV_DESKTOP, ACT_APPLICATION_WINDOWS, ACT_MISSION_CONTROL, ACT_NEXT_DESKTOP, XXXXXXX, XXXXXXX,
         XXXXXXX, APP_MAIL, APP_XCODE, XXXXXXX, XXXXXXX, ACT_CLIPBOARD,        ACT_NEW_NOTE, ACT_MAXIMIZE_WINDOW, ACT_SHOW_NOTIFICATIONS, ACT_SHOW_DESKTOP, XXXXXXX, XXXXXXX,
         XXXXXXX, XXXXXXX,                                                     _______, _______,
-        _______, _______, _______,                                            _______, ACT_MENU_BAR, ACT_SHORTCAT
+        _______, _______, _______,                                            _______, ACT_MENU_BAR, ACT_SWITCH_WINDOWS
     ),
     [ACTION_LAYER] = LAYOUT(
         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
