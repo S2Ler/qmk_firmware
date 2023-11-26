3907a5d44c8fd300aec9774bb43435fa00caa416
Aliaksandr Bialiauski <413986+S2Ler@users.noreply.github.com>
2023-11-26 07:32:08 +0300

    wip

--- keyboards/ergohaven/hpd/keymaps/s2ler/layers.h
+++ keyboards/ergohaven/hpd/keymaps/s2ler/layers.h
@@ -5,15 +5,17 @@
 #ifndef QMK_FIRMWARE_LAYERS_H
 #define QMK_FIRMWARE_LAYERS_H
 
-#define HOME_LAYER 0
-#define SYMBAL_LAYER 1
-#define NAV_LAYER 2
-#define MOUSE_LAYER 3
-#define APP_LAYER 4
-#define ACTION_LAYER 5
-#define NUM_LAYER 6
-#define F_LAYER 7
-#define LAYERS_COUNT 8
+#define HOME_LAYER_EN 0
+#define HOME_LAYER_RU 1
+#define SYMBAL_LAYER_EN 2
+#define SYMBAL_LAYER_RU 3
+#define NAV_LAYER 4
+#define MOUSE_LAYER 5
+#define APP_LAYER 6
+#define ACTION_LAYER 7
+#define NUM_LAYER 8
+#define F_LAYER 9
+#define LAYERS_COUNT 10
 
 
 #endif // QMK_FIRMWARE_LAYERS_H
--- keyboards/ergohaven/remnant/keymaps/s2ler/keymap.c
+++ keyboards/ergohaven/remnant/keymaps/s2ler/keymap.c
@@ -10,12 +10,19 @@
 
 #define LANG_COUNT 2
 
+#define EN_LANG_IDX 1
+#define RU_LANG_IDX 2
+
+void set_language_idx(uint8_t lang_idx);
+
 void keyboard_post_init_user(void) {
+    debug_enable = true;
     // Customise these values to desired behaviour
-    debug_enable=true;
-//    debug_matrix=true;
-//    debug_keyboard=true;
-//    debug_mouse=true;
+    //    debug_matrix=true;
+    //    debug_keyboard=true;
+    //    debug_mouse=true;
+
+    set_language_idx(EN_LANG_IDX);
 }
 
 // MARK: - Combos
@@ -25,7 +32,7 @@ enum combo_events {
     NEW_LINE_ABOVE_COMBO,
 };
 
-const uint16_t PROGMEM new_line_combo[] = {KC_F, KC_ENTER, COMBO_END};
+const uint16_t PROGMEM new_line_combo[]       = {KC_F, KC_ENTER, COMBO_END};
 const uint16_t PROGMEM new_line_above_combo[] = {KC_D, KC_ENTER, COMBO_END};
 
 void tap_new_line(void) {
@@ -44,8 +51,8 @@ combo_t key_combos[] = {
     [NEW_LINE_ABOVE_COMBO] = COMBO_ACTION(new_line_above_combo),
 };
 
-void process_combo_event(uint16_t combo_index, bool pressed) {
-    switch(combo_index) {
+void process_combo_event(const uint16_t combo_index, const bool pressed) {
+    switch (combo_index) {
         case NEW_LINE_COMBO:
             if (pressed) {
                 tap_new_line();
@@ -70,93 +77,113 @@ enum custom_codes {
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [HOME_LAYER] = LAYOUT(
-        LGUI(KC_TAB), XXXXXXX, SH_RAW_PASTER, REDO, ACT_SELECTION_SCREENSHOT, TD(TD_SAFE_BOOT),     TD(TD_SAFE_BOOT), LANG_SWITCH, XXXXXXX, XXXXXXX, XXXXXXX, ACT_SWITCH_WINDOWS,
+    [HOME_LAYER_EN] = LAYOUT(
+        LGUI(KC_TAB), XXXXXXX, SH_RAW_PASTER, REDO, ACT_SELECTION_SCREENSHOT, TD(TD_SAFE_BOOT), TD(TD_SAFE_BOOT), LANG_SWITCH, XXXXXXX, XXXXXXX, XXXXXXX, ACT_SWITCH_WINDOWS,
 
-        KC_TAB,  KC_Q,  KC_W, KC_E,  KC_R, KC_T,                                                    KC_Y, KC_U, KC_I, KC_O, KC_P, OSL(ACTION_LAYER),
-        KC_LCTL, KC_A, KC_S, KC_D, KC_F, KC_G,                                                      KC_H, KC_J, KC_K, KC_L, LSFT_T(KC_ESC), LSFT(KC_RSFT),
-        KC_LSFT, KC_Z,  KC_X, KC_C,  KC_V, KC_B,                                                    KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_BSLS,
+        KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, OSL(ACTION_LAYER),
+        KC_LCTL, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, LSFT_T(KC_ESC), LSFT(KC_RSFT),
+        KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_BSLS,
 
-        KC_LALT, KC_DELETE,                                                                         KC_BSPC, QK_LEAD,
-        QK_LGUI, TL_LOWR, MO(APP_LAYER),                                                            KC_ENT, TL_UPPR, KC_SPACE
-    ),
+        KC_LALT, KC_DELETE, KC_BSPC, QK_LEAD,
+        QK_LGUI, TL_LOWR, MO(APP_LAYER), KC_ENT, TL_UPPR, KC_SPACE
+        ),
+    [HOME_LAYER_RU] = LAYOUT(
+        LGUI(KC_TAB), XXXXXXX, SH_RAW_PASTER, REDO, ACT_SELECTION_SCREENSHOT, TD(TD_SAFE_BOOT), TD(TD_SAFE_BOOT), LANG_SWITCH, XXXXXXX, XXXXXXX, XXXXXXX, ACT_SWITCH_WINDOWS,
 
-    [SYMBAL_LAYER] = LAYOUT(
-        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                                 XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-        _______, LSFT(KC_1), LSFT(KC_2), LSFT(KC_3), LSFT(KC_4), LSFT(KC_5),                  LSFT(KC_6), LSFT(KC_7), LSFT(KC_8), LSFT(KC_9), LSFT(KC_0), LSFT(KC_EQUAL),
-        _______, KC_GRAVE, LSFT(KC_QUOTE), KC_SCLN, LSFT(KC_LBRC), KC_LBRC,                         KC_RBRC, LSFT(KC_RBRC), KC_MINUS, KC_EQUAL, LSFT(KC_SCLN), XXXXXXX,
-        _______, LSFT(KC_GRAVE), KC_QUOTE, MO(NUM_LAYER), LSFT(KC_9), XXXXXXX,    LSFT(KC_MINUS), LSFT(KC_0), LSFT(KC_COMMA), LSFT(KC_DOT), LSFT(KC_SLASH), LSFT(KC_BSLS),
-                           _______, _______,                                                  _______, _______,
-                                         _______, _______, _______,      _______, _______,  _______
+        KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, OSL(ACTION_LAYER),
+        KC_LCTL, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, LSFT_T(KC_ESC), LSFT(KC_RSFT),
+        KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_BSLS,
 
-    ),
+        KC_LALT, KC_DELETE, KC_BSPC, QK_LEAD,
+        QK_LGUI, TL_LOWR, MO(APP_LAYER), KC_ENT, TL_UPPR, KC_SPACE
+        ),
+
+    [SYMBAL_LAYER_EN] = LAYOUT(
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+        _______, LSFT(KC_1), LSFT(KC_2), LSFT(KC_3), LSFT(KC_4), LSFT(KC_5), LSFT(KC_6), LSFT(KC_7), LSFT(KC_8), LSFT(KC_9), LSFT(KC_0), LSFT(KC_EQUAL),
+        _______, KC_GRAVE, LSFT(KC_QUOTE), KC_SCLN, LSFT(KC_LBRC), KC_LBRC, KC_RBRC, LSFT(KC_RBRC), KC_MINUS, KC_EQUAL, LSFT(KC_SCLN), XXXXXXX,
+        _______, LSFT(KC_GRAVE), KC_QUOTE, MO(NUM_LAYER), LSFT(KC_9), XXXXXXX, LSFT(KC_MINUS), LSFT(KC_0), LSFT(KC_COMMA), LSFT(KC_DOT), LSFT(KC_SLASH), LSFT(KC_BSLS),
+        _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______
+
+        ),
+    [SYMBAL_LAYER_RU] = LAYOUT(
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+        _______, LSFT(KC_1), LSFT(KC_2), LSFT(KC_3), LSFT(KC_4), LSFT(KC_5), LSFT(KC_6), LSFT(KC_7), LSFT(KC_8), LSFT(KC_9), LSFT(KC_0), LSFT(KC_EQUAL),
+        _______, KC_GRAVE, LSFT(KC_QUOTE), KC_SCLN, LSFT(KC_LBRC), KC_LBRC, KC_RBRC, LSFT(KC_RBRC), KC_MINUS, KC_EQUAL, LSFT(KC_SCLN), XXXXXXX,
+        _______, LSFT(KC_GRAVE), KC_QUOTE, MO(NUM_LAYER), LSFT(KC_9), XXXXXXX, LSFT(KC_MINUS), LSFT(KC_0), LSFT(KC_COMMA), LSFT(KC_DOT), LSFT(KC_SLASH), LSFT(KC_BSLS),
+        _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______
+        ),
 
     [NAV_LAYER] = LAYOUT(
-        QK_REPEAT_KEY, XXXXXXX, XXXXXXX, XXXXXXX, RS_CENTER_HALF, XXXXXXX,                       RS_LEFT_HALF, RS_BOTTOM_HALF, RS_TOP_HALF, RS_RIGHT_HALF, XXXXXXX, XXXXXXX,
-        _______, XXXXXXX, REINDENT, MOVE_LINE_DOWN, MOVE_LINE_UP, XXXXXXX,                KC_PGDN, LINE_START, LINE_END, KC_PGUP, SELECT_WORD,  SHOW_DOCUMENTATION,
-        _______, KC_LSFT, FIND_CALLERS, NAV_BACK, NAV_FORWARD, TO_DEFINITION,          KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT, NEXT_APP_WINDOW,  FILE_STRUCTURE,
-        _______, LSA_T(KC_ENTER), REVEAL_IN, SH_RENAME, DELETE_LINE, NEXT_PLACEHOLDER,      PREV_TAB, PREV_WINDOW_TAB, NEXT_WINDOW_TAB, NEXT_TAB, XXXXXXX,  XCODE_ACTION_POPUP,
-                           _______, _______,                                              _______, _______,
-                                             _______, _______, _______,           _______, _______, _______
-    ),
+        QK_REPEAT_KEY, XXXXXXX, XXXXXXX, XXXXXXX, RS_CENTER_HALF, XXXXXXX, RS_LEFT_HALF, RS_BOTTOM_HALF, RS_TOP_HALF, RS_RIGHT_HALF, XXXXXXX, XXXXXXX,
+        _______, XXXXXXX, REINDENT, MOVE_LINE_DOWN, MOVE_LINE_UP, XXXXXXX, KC_PGDN, LINE_START, LINE_END, KC_PGUP, SELECT_WORD, SHOW_DOCUMENTATION,
+        _______, KC_LSFT, FIND_CALLERS, NAV_BACK, NAV_FORWARD, TO_DEFINITION, KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT, NEXT_APP_WINDOW, FILE_STRUCTURE,
+        _______, LSA_T(KC_ENTER), REVEAL_IN, SH_RENAME, DELETE_LINE, NEXT_PLACEHOLDER, PREV_TAB, PREV_WINDOW_TAB, NEXT_WINDOW_TAB, NEXT_TAB, XXXXXXX, XCODE_ACTION_POPUP,
+        _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______
+        ),
 
     [MOUSE_LAYER] = LAYOUT(
-        RGB_TOG, QK_MAGIC_SWAP_CTL_GUI, QK_MAGIC_UNSWAP_CTL_GUI, XXXXXXX, XXXXXXX, XXXXXXX,                 XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-        _______, XXXXXXX, DM_RSTP, LGUI(KC_BTN1), KC_BTN3, KC_MUTE,           KC_WH_R, KC_WH_U, KC_WH_D, KC_WH_L, XXXXXXX, XXXXXXX,
-        _______, DM_PLY1, DM_REC1, KC_BTN1, KC_BTN2, KC_VOLU,                 KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, XXXXXXX, XXXXXXX,
-        _______, DM_PLY2, DM_REC2, XXXXXXX, MV_TO_PREV_WINDOW, KC_VOLD,       _______, MV_TO_NEXT_WINDOW, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-        _______, _______,                                                     ZOOM_OUT, ZOOM_IN,
-                                         _______, _______, _______,       _______, _______,  _______
-    ),
+        RGB_TOG, QK_MAGIC_SWAP_CTL_GUI, QK_MAGIC_UNSWAP_CTL_GUI, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+        _______, XXXXXXX, DM_RSTP, LGUI(KC_BTN1), KC_BTN3, KC_MUTE, KC_WH_R, KC_WH_U, KC_WH_D, KC_WH_L, XXXXXXX, XXXXXXX,
+        _______, DM_PLY1, DM_REC1, KC_BTN1, KC_BTN2, KC_VOLU, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, XXXXXXX, XXXXXXX,
+        _______, DM_PLY2, DM_REC2, XXXXXXX, MV_TO_PREV_WINDOW, KC_VOLD, _______, MV_TO_NEXT_WINDOW, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+        _______, _______, ZOOM_OUT, ZOOM_IN,
+        _______, _______, _______, _______, _______, _______
+        ),
     [APP_LAYER] = LAYOUT(
-        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                 XXXXXXX, SAF_PERSONAL, SAF_YANDEX, SAF_YANDEX_APP_METRICA, SAF_ALIKE, XXXXXXX,
-        XXXXXXX, APP_GIT_GUI, APP_SLACK, XXXXXXX, ACT_SNIPPETS, ACT_TODO,     XXXXXXX, APP_TERMINAL, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-        XXXXXXX, APP_PYCHARM, APP_BROWSER, XXXXXXX, ACT_SIRI, ACT_DICTATION,  ACT_PREV_DESKTOP, ACT_APPLICATION_WINDOWS, ACT_MISSION_CONTROL, ACT_NEXT_DESKTOP, XXXXXXX, XXXXXXX,
-        XXXXXXX, APP_MAIL, APP_XCODE, XXXXXXX, XXXXXXX, ACT_CLIPBOARD,        ACT_NEW_NOTE, ACT_MAXIMIZE_WINDOW, ACT_SHOW_NOTIFICATIONS, ACT_SHOW_DESKTOP, XXXXXXX, XXXXXXX,
-        XXXXXXX, XXXXXXX,                                                     _______, _______,
-        _______, _______, _______,                                            _______, ACT_MENU_BAR, ACT_SHORTCAT
-    ),
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, SAF_PERSONAL, SAF_YANDEX, SAF_YANDEX_APP_METRICA, SAF_ALIKE, XXXXXXX,
+        XXXXXXX, APP_GIT_GUI, APP_SLACK, XXXXXXX, ACT_SNIPPETS, ACT_TODO, XXXXXXX, APP_TERMINAL, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+        XXXXXXX, APP_PYCHARM, APP_BROWSER, XXXXXXX, ACT_SIRI, ACT_DICTATION, ACT_PREV_DESKTOP, ACT_APPLICATION_WINDOWS, ACT_MISSION_CONTROL, ACT_NEXT_DESKTOP, XXXXXXX, XXXXXXX,
+        XXXXXXX, APP_MAIL, APP_XCODE, XXXXXXX, XXXXXXX, ACT_CLIPBOARD, ACT_NEW_NOTE, ACT_MAXIMIZE_WINDOW, ACT_SHOW_NOTIFICATIONS, ACT_SHOW_DESKTOP, XXXXXXX, XXXXXXX,
+        XXXXXXX, XXXXXXX, _______, _______,
+        _______, _______, _______, _______, ACT_MENU_BAR, ACT_SHORTCAT
+        ),
     [ACTION_LAYER] = LAYOUT(
-        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-        XXXXXXX, ACT_RAYCAST_IMPROVE_WRITING, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+        XXXXXXX, ACT_RAYCAST_IMPROVE_WRITING, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
         XXXXXXX, ACT_RAYCAST_AI, XXXXXXX, XXXXXXX, ACT_SEARCH_FILES, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-        XXXXXXX, ACT_RAYCAST_FIX_SPELLING, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-        XXXXXXX, XXXXXXX,                                                           XXXXXXX, XXXXXXX,
-        XXXXXXX, XXXXXXX, XXXXXXX,                                                  XXXXXXX, XXXXXXX,  XXXXXXX
-    ),
+        XXXXXXX, ACT_RAYCAST_FIX_SPELLING, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
+        ),
     [NUM_LAYER] = LAYOUT(
-        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                       XXXXXXX,     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX,
-        _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                       XXXXXXX,     KC_7, KC_8, KC_9, KC_COMMA, KC_KP_PLUS,
-        _______, MO(F_LAYER), XXXXXXX, XXXXXXX, KC_LALT, XXXXXXX,                       KC_KP_COMMA, KC_4, KC_5, KC_6, KC_0,     KC_KP_EQUAL,
-        _______, KC_LSFT, KC_LCTL, XXXXXXX, KC_LGUI, XXXXXXX,                       KC_DOT, KC_1, KC_2, KC_3, KC_SLASH, XXXXXXX,
-        XXXXXXX, XXXXXXX,                                                           _______, _______,
-        XXXXXXX, XXXXXXX, _______,                                                  _______, XXXXXXX,  XXXXXXX
-    ),
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+        _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_7, KC_8, KC_9, KC_COMMA, KC_KP_PLUS,
+        _______, MO(F_LAYER), XXXXXXX, XXXXXXX, KC_LALT, XXXXXXX, KC_KP_COMMA, KC_4, KC_5, KC_6, KC_0, KC_KP_EQUAL,
+        _______, KC_LSFT, KC_LCTL, XXXXXXX, KC_LGUI, XXXXXXX, KC_DOT, KC_1, KC_2, KC_3, KC_SLASH, XXXXXXX,
+        XXXXXXX, XXXXXXX, _______, _______,
+        XXXXXXX, XXXXXXX, _______, _______, XXXXXXX, XXXXXXX
+        ),
     [F_LAYER] = LAYOUT(
-        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX,
-        _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                       XXXXXXX, KC_F7, KC_F8, KC_F9, KC_F11, XXXXXXX,
-        _______, XXXXXXX, XXXXXXX, XXXXXXX, _______, XXXXXXX,                       XXXXXXX, KC_F4, KC_F5, KC_F6, KC_F10,     XXXXXXX,
-        _______, _______, _______, XXXXXXX, _______, XXXXXXX,                       XXXXXXX, KC_F1, KC_F2, KC_F3, XXXXXXX, XXXXXXX,
-        XXXXXXX, XXXXXXX,                                                           _______, _______,
-        XXXXXXX, XXXXXXX, _______,                                                  _______, XXXXXXX,  XXXXXXX
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+        _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_F7, KC_F8, KC_F9, KC_F11, XXXXXXX,
+        _______, XXXXXXX, XXXXXXX, XXXXXXX, _______, XXXXXXX, XXXXXXX, KC_F4, KC_F5, KC_F6, KC_F10, XXXXXXX,
+        _______, _______, _______, XXXXXXX, _______, XXXXXXX, XXXXXXX, KC_F1, KC_F2, KC_F3, XXXXXXX, XXXXXXX,
+        XXXXXXX, XXXXXXX, _______, _______,
+        XXXXXXX, XXXXXXX, _______, _______, XXXXXXX, XXXXXXX
         ),
 };
 
 bool process_record_user(const uint16_t keycode, keyrecord_t *record) {
-    if (!process_select_word(keycode, record, SELECT_WORD)) { return false; }
+    if (!process_select_word(keycode, record, SELECT_WORD)) {
+        return false;
+    }
 
     switch (keycode) {
         case SELECT_LINE:
             if (record->event.pressed) {
                 tap_code16(LINE_END);
-                tap_code16( SELECT_LINE_START);
+                tap_code16(SELECT_LINE_START);
                 return false;
             }
             break;
         case DELETE_LINE:
             if (record->event.pressed) {
                 tap_code16(LINE_START);
-                tap_code16( KILL_TO_LINE_END);
+                tap_code16(KILL_TO_LINE_END);
                 return false;
             }
             break;
@@ -171,11 +198,26 @@ bool process_record_user(const uint16_t keycode, keyrecord_t *record) {
 // MARK: Raw HID
 
 uint8_t get_language_idx(void) {
+    if (IS_LAYER_ON(HOME_LAYER_RU)) {
+        return 2;
+    } else if (IS_LAYER_ON(HOME_LAYER_EN)) {
+        return 1;
+    }
     return 1;
 }
 
-void set_language_idx(uint8_t lang_idx) {
-    //
+void set_language_idx(const uint8_t lang_idx) {
+    if (lang_idx == 1) {
+        layer_off(HOME_LAYER_RU);
+        layer_on(HOME_LAYER_EN);
+        layer_off(SYMBAL_LAYER_RU);
+        layer_on(SYMBAL_LAYER_EN);
+    } else if (lang_idx == 2) {
+        layer_off(HOME_LAYER_EN);
+        layer_on(HOME_LAYER_RU);
+        layer_off(SYMBAL_LAYER_EN);
+        layer_on(SYMBAL_LAYER_RU);
+    }
 }
 
 void raw_hid_receive(uint8_t *data, uint8_t length) {
@@ -189,7 +231,7 @@ void raw_hid_receive(uint8_t *data, uint8_t length) {
     }
 
     uint8_t report_id = data[0];
-    uint8_t cmd_id = data[1];
+    uint8_t cmd_id    = data[1];
 
     if (report_id == 1) {
         switch (cmd_id) {
@@ -270,7 +312,7 @@ void raw_hid_receive(uint8_t *data, uint8_t length) {
                 raw_hid_send(response, sizeof(response));
                 print("raw_hid_receive: Unsupported cmd");
             }
-                break;
+            break;
         }
     } else {
         uint8_t response[] = {3, 255, 255};
@@ -289,25 +331,20 @@ void raw_hid_receive(uint8_t *data, uint8_t length) {
 // MARK: Leader key
 
 void leader_start_user(void) {
-
 }
 
 void leader_end_user(void) {
     if (leader_sequence_one_key(KC_F)) {
         // Leader, f => Types the below string
         tap_new_line();
-    }
-    else if (leader_sequence_one_key(KC_D)) {
+    } else if (leader_sequence_one_key(KC_D)) {
         // Leader, d, d => Ctrl+A, Ctrl+C
         tap_new_line_above();
-    }
-    else if (leader_sequence_two_keys(KC_G, KC_A)) {
+    } else if (leader_sequence_two_keys(KC_G, KC_A)) {
         send_string("git add -p && git commit --amend --no-edit && git push --force");
-    }
-    else if (leader_sequence_three_keys(KC_G, KC_H, KC_P)) {
+    } else if (leader_sequence_three_keys(KC_G, KC_H, KC_P)) {
         send_string("gh-pr-then-open");
-    }
-    else if (leader_sequence_one_key(KC_O)) {
+    } else if (leader_sequence_one_key(KC_O)) {
         tap_code16(QK_LSFT | QK_LGUI | KC_O);
     }
 }
